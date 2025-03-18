db987ad6a83b15c7c7c9c5ba1de20dc91735a795
Daniel Nozka <danozka@gmail.com>
2025-03-18 20:51:00 +0100

    Improves logging settings

--- src/settings.py
+++ src/settings.py
@@ -7,17 +7,17 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     logging_level: str = Field(alias='LOGGING_LEVEL', default='INFO')
+    logging_config: dict[str, Any] | None = None
     app_host: str = Field(alias='APP_HOST', default='0.0.0.0')
     app_port: int = Field(alias='APP_HOST', default=8000)
     json_web_token_secret_key: str = Field(alias='JWT_SECRET_KEY', default='my_secret_key')
     json_web_token_algorithm: str = Field(alias='JWT_ALGORITHM', default='HS256')
     json_web_token_expiration_days: int = Field(alias='JWT_ACCESS_TOKEN_EXPIRATION_DAYS', default=7)
     database_path: Path = Field(alias='DATABASE_PATH', default=Path('./data/audio-nest.db'))
-    logging_config: dict[str, Any] | None = None
 
     def __init__(self) -> None:
         super().__init__()
-        self.logging_config = {
+        self.logging_config = self.logging_config or {
             'disable_existing_loggers': False,
             'formatters': {
                 'simple': {
